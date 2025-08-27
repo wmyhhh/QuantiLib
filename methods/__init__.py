51edb8141418144d0cb058f62e28b19bb51b3598
wmyhhh <041125@sjtu.edu.cn>
2025-08-27 10:39:59 +0800

    first commit

--- methods/__init__.py
+++ methods/__init__.py
@@ -1,12 +1,11 @@
-# llm_quant/methods/__init__.py
 from .bnb import BnBQuantizer
 from .gptq import GPTQQuantizer
 from .aqlm import AQLMQuantizer
 from .awq import AWQQuantizer
 
-__all__ = [
-    "BnBQuantizer",
-    "GPTQQuantizer",
-    "AQLMQuantizer",
-    "AWQQuantizer",
-]
+QUANTIZER_REGISTRY = {
+    "bnb": BnBQuantizer,
+    "gptq": GPTQQuantizer,
+    "aqlm": AQLMQuantizer,
+    "awq": AWQQuantizer
+}
